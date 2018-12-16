5eb6e202a374dd633ff1e5fe9f8896b555946dcc
ForiDunk <Szabolcs.forreiter@gmail.com>
2018-12-16 11:55:58 +0200

    install axios

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 import Title from './components/Title';
 import SendMessageForm from './components/SendMessageForm';
 import MessagesList from './components/MessagesList';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount = () => {
+    axios.get('http://assignment.bunq.com/users')
+      .then(response => this.setState({ users: response.data }));
+  }
+
   render() {
     return (
       <div className="App">
