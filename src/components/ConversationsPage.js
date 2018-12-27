a1fa6577c97fb67e44c59b3bc4ba7be5d8f35d86
bela-dev <fori-prg@pathwwway.com>
2018-12-27 15:52:22 +0200

    conversation page style

--- src/components/ConversationsPage.js
+++ src/components/ConversationsPage.js
@@ -2,6 +2,37 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getConversations, selectConversation } from '../actions';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import GroupTwoTone from '@material-ui/icons/GroupTwoTone';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const styles = theme => {
+  return ({
+    paper: {
+      width          : '100%',
+      maxWidth       : 700,
+      margin         : '0 auto',
+      textAlign      : 'center',
+    },
+    headingText: {
+      backgroundColor: theme.palette.primary.dark,
+      padding        : '5px 0', 
+      color          : theme.palette.getContrastText(theme.palette.primary.dark),
+    },
+    link: {
+      textDecoration: 'none'
+    },
+    progress: {
+      margin: theme.spacing.unit * 2,
+    },
+  })
+};
 
 class ConversationsPage extends React.Component {
   componentDidMount() {
@@ -13,21 +44,27 @@ class ConversationsPage extends React.Component {
   }
 
   render() {
+    const { classes } = this.props;
     const allConversations = this.props.conversations.map((conversation, index) => {
       const path = `/conversations/${conversation.conversation.conversationId}`;
 
       return (
         <Link to={path} onClick={() => this.onConversationSelect(conversation)} key={index}>
-          <div>{conversation.conversation.name ? conversation.conversation.name : 'undefined'}</div>
+          <ListItem button divider>
+            <ListItemIcon><GroupTwoTone color="primary" fontSize="large"/></ListItemIcon>
+            <ListItemText>{conversation.conversation.name ? conversation.conversation.name : 'undefined'}</ListItemText>
+          </ListItem>
         </Link>
       );
     });
 
     return (
-      <div>
-        <h3>Select a Conversation:</h3>
-        {allConversations}
-      </div>
+      <Paper className={classes.paper} elevation={1}>
+        <Typography className={classes.headingText} variant="h5">Select a Conversation:</Typography>
+        <List disablePadding>
+          {this.props.conversations.length === 0 ? <CircularProgress className={classes.progress}/> : allConversations}
+        </List>
+      </Paper>
     );
   }
 }
@@ -40,4 +77,4 @@ const mapStateToProps = (state) => {
   };
 }
 
-export default connect(mapStateToProps, { getConversations , selectConversation })(ConversationsPage);
+export default connect(mapStateToProps, { getConversations , selectConversation })(withStyles(styles)(ConversationsPage));
