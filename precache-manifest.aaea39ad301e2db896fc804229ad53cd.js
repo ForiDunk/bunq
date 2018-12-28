820c3e0ccb6d37e2836c940e1b2438ce674e30f3
bela-dev <fori-prg@pathwwway.com>
2018-12-28 12:41:24 +0200

    Updates

---
+++ precache-manifest.aaea39ad301e2db896fc804229ad53cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09fac6c5968909e7724e",
+    "url": "/bunq/static/css/main.5705cb83.chunk.css"
+  },
+  {
+    "revision": "09fac6c5968909e7724e",
+    "url": "/bunq/static/js/main.09fac6c5.chunk.js"
+  },
+  {
+    "revision": "3e2566af062f4abf3594",
+    "url": "/bunq/static/js/1.3e2566af.chunk.js"
+  },
+  {
+    "revision": "9a86a31195726981ea74",
+    "url": "/bunq/static/js/runtime~main.9a86a311.js"
+  },
+  {
+    "revision": "7de6d2f16cde3a9c61e8199d9f693415",
+    "url": "/bunq/index.html"
+  }
+];
